782e2e8276b166654a21a0b2efb5904798341fcb
MaTo <ily4368@gmail.com>
2023-06-01 21:31:20 +0300

    Start with Default Vue Template Checking vue thins with test component

---
+++ src/components/Test.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { reactive } from 'vue';
+
+
+const state = reactive({ count: 0 })
+
+
+const increment = () => state.count++
+</script>
+
+<template>
+    <button @click="increment">{{ state.count }}</button>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import Test from '@/components/Test.vue';
+import { nextTick, ref } from 'vue';
+
+
+</script>
+
+<template>
+  <main>
+    <Test>div</Test>
+  </main>
+</template>
