2b3ceae6dfe12f311f23303889f4f94dce5adbaa
MaTo <ily4368@gmail.com>
2023-06-04 19:15:59 +0300

    Add Select Locale Component to home view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,15 @@
 <script setup lang="ts">
+import LocaleSelector from '@/components/LocaleSelector.vue'
 import ThemeToggle from '@/components/ThemeToggle.vue'
+import { useI18n } from 'vue-i18n'
+
+const { t } = useI18n()
 </script>
 
 <template>
   <main>
     <theme-toggle />
-    <v-btn class="bg-orange-darken-1">{{ $t('test') }}</v-btn>
+    <locale-selector />
+    <v-btn class="bg-orange-darken-1">{{ t('test') }}</v-btn>
   </main>
 </template>
