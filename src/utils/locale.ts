a512535d1ae5d32947a47f51ac272c26cea9bf7b
MaTo <ily4368@gmail.com>
2023-06-04 17:59:00 +0300

    GetDefaultLocale function

---
+++ src/utils/locale.ts
@@ -0,0 +1,35 @@
+import { AppLocale } from '@/types/locale'
+import { LocalStorageKey } from './localStorage'
+
+export const getBrowserLocale = () => {
+  const locale =
+    navigator.language ||
+    (navigator as any).browserLanguage ||
+    (navigator.languages || [AppLocale.en])[0]
+
+  if (/^ru\b/i.test(locale)) {
+    return AppLocale.ru
+  }
+
+  if (/^en\b/i.test(locale)) {
+    return AppLocale.en
+  }
+
+  return AppLocale.en
+}
+
+export const getLocalLocale = () => {
+  const locale = localStorage.getItem(LocalStorageKey.locale)
+  return Object.values(AppLocale).includes(locale as any) ? (locale as AppLocale) : null
+}
+
+export const setDefaultLocale = (newLocale: AppLocale) => {
+  localStorage.setItem(LocalStorageKey.locale, newLocale)
+}
+
+export const getDefaultLocale = () => {
+  const localLocale = getLocalLocale()
+  if (localLocale) return localLocale
+
+  return getBrowserLocale()
+}
