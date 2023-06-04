947e3cc07ef19922febbf1d7c3b95fca0263375d
MaTo <ily4368@gmail.com>
2023-06-04 19:15:15 +0300

    New UI Button to change locale

---
+++ src/components/ui/SelectLocaleButton.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { AppLocale, LanguageName } from '@/types/locale'
+
+const props = defineProps<{
+  locale: AppLocale
+}>()
+const emit = defineEmits<{ (e: 'locale-change', newLocale: AppLocale): void }>()
+
+const handleOnClick = (newLocale: AppLocale) => {
+  if (props.locale === newLocale) return
+  emit('locale-change', newLocale)
+}
+</script>
+
+<template>
+  <v-menu>
+    <template #activator="{ props }">
+      <v-btn data-testid="open-menu" size="small" v-bind="props" icon="mdi:mdi-translate"></v-btn>
+    </template>
+    <v-list density="compact" nav>
+      <v-list-item
+        color="primary"
+        min-height="30"
+        v-for="item in AppLocale"
+        :key="item"
+        :value="item"
+        :active="locale === item"
+        @click="() => handleOnClick(item)"
+      >
+        <v-list-item-title>{{ LanguageName[item] }}</v-list-item-title>
+      </v-list-item>
+    </v-list>
+  </v-menu>
+</template>
