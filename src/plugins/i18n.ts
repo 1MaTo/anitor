2eff4048d5bfde4de5690bfe62bfada32dc73954
MaTo <ily4368@gmail.com>
2023-06-04 18:00:00 +0300

    Define default locale on startup

--- src/plugins/i18n.ts
+++ src/plugins/i18n.ts
@@ -2,12 +2,13 @@ import { createI18n } from 'vue-i18n'
 import en from '@/locales/en.json'
 import ru from '@/locales/ru.json'
 import { AppLocale } from '@/types/locale'
+import { getDefaultLocale } from '@/utils/locale'
 
 type MessageSchema = typeof en
 
 const i18n = createI18n<[MessageSchema], AppLocale>({
   legacy: false,
-  locale: AppLocale.en,
+  locale: getDefaultLocale(),
   fallbackLocale: AppLocale.en,
   messages: {
     en: en,
