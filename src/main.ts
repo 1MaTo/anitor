1cdc265d19ea27478d6fde70b2ecfd46adefa4ae
MaTo <ily4368@gmail.com>
2023-06-04 16:08:46 +0300

    Format file

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,9 @@ import { createPinia } from 'pinia'
 import { createApp } from 'vue'
 
 import App from './App.vue'
+import i18n from './plugins/i18n'
 import router from './plugins/router'
 import vuetify from './plugins/vuetify'
-import i18n from './plugins/i18n'
 
 const app = createApp(App)
 
