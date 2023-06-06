bb689e351039da1d0f23b767dcecb050026f726c
MaTo <ily4368@gmail.com>
2023-06-06 23:36:56 +0300

    Make App name constant

--- src/components/ui/AppLogo.vue
+++ src/components/ui/AppLogo.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
 import logoImage from '@/assets/bd_logo.png'
+import { APP_NAME } from '@/utils/app'
 </script>
 
 <template>
   <div class="d-flex container">
     <v-img class="mr-3" width="60" aspect-ratio="1/1" :src="logoImage"></v-img>
-    <div class="text-h5">BD checker</div>
+    <div class="text-h5">{{ APP_NAME }}</div>
   </div>
 </template>
 
