e71e4a350cdf1823591139b77ae302b1a5e59bad
MaTo <ily4368@gmail.com>
2023-06-06 23:22:55 +0300

    Remove debug code

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,8 @@
 <script setup lang="ts">
-import LocaleSelector from '@/components/LocaleSelector.vue'
 import ThemeToggle from '@/components/ThemeToggle.vue'
 import { useI18n } from 'vue-i18n'
 
 const { t } = useI18n()
 </script>
 
-<template>
-  <main>
-    <theme-toggle />
-    <locale-selector />
-    <v-btn class="bg-orange-darken-1">{{ t('test') }}</v-btn>
-  </main>
-</template>
+<template></template>
