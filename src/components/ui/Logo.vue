b87504265a46e38f907a734034f4ed9f8d9dbc1c
MaTo <ily4368@gmail.com>
2023-06-06 23:22:15 +0300

    Logo component

---
+++ src/components/ui/Logo.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import logoImage from '@/assets/bd_logo.png'
+</script>
+
+<template>
+  <div class="d-flex container">
+    <v-img class="mr-3" width="60" aspect-ratio="1/1" :src="logoImage"></v-img>
+    <div class="text-h5">BD checker</div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+    cursor: default;
+}
+</style>
