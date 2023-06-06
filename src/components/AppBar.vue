f2291a8ae33b7261eb77977a7cbd398687a5052b
MaTo <ily4368@gmail.com>
2023-06-06 23:23:55 +0300

    New AppBar component

---
+++ src/components/AppBar.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import SettingBar from './SettingBar.vue'
+import Logo from './ui/Logo.vue'
+</script>
+
+<template>
+  <v-app-bar>
+    <v-row justify="space-between" align="center">
+      <v-col cols="auto" class="mx-4">
+        <logo />
+      </v-col>
+      <v-col cols="auto" class="mx-4">
+        <setting-bar />
+      </v-col>
+    </v-row>
+  </v-app-bar>
+</template>
+
+<style scoped></style>
