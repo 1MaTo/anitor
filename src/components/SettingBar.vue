e97d8b9ce6db87a74c9c183a5b0a4f4af831468a
MaTo <ily4368@gmail.com>
2023-06-06 23:23:37 +0300

    New SettingBar component

---
+++ src/components/SettingBar.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import LocaleSelector from '@/components/LocaleSelector.vue'
+import ThemeToggle from './ThemeToggle.vue'
+</script>
+
+<template>
+  <div class="container">
+    <div class="item">
+      <locale-selector />
+    </div>
+    <div class="item">
+      <theme-toggle />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+}
+
+.item:not(:first-child) {
+  margin-left: 8px;
+}
+</style>
