d2f73d17aab4785054b78fd6fca59eca562e42b7
MaTo <ily4368@gmail.com>
2023-06-11 18:06:03 +0300

    SearchField development

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,27 @@
 <script setup lang="ts">
-const handleGetApi = async () => {
-  const result = await $fetch('/api/nyaa/search')
-  console.log(result)
-}
+import SearchField from '~/components/ui/SearchField.vue'
+
+const inputValue = ref()
+const loading = ref(false)
 </script>
 
 <template>
   <div>
-    {{ $t('test') }}
-    <v-btn @click="handleGetApi">get api</v-btn>
+    <v-container>
+      <v-row>
+        <v-col>
+          <client-only>
+            <search-field
+              v-model="inputValue"
+              :placeholder="$t('search-torrents')"
+              :loading="loading"
+              highlight-on-focus
+              size="big"
+              @submit="console.log('submit')"
+            />
+          </client-only>
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
