a7c692285ed27b6651929d345242d14199e932d0
MaTo <ily4368@gmail.com>
2023-06-12 19:35:08 +0300

    Change locale file format New values in locales

---
+++ locales/en.ts
@@ -0,0 +1,20 @@
+const strings = {
+  test: 'English locale',
+  'search-torrents': 'Search torrents',
+  nyaa: {
+    'sub-category': {
+      '1_1': 'AMV (Music video)',
+      '1_2': 'English',
+      '1_3': 'Non English translated',
+      '1_4': 'Raw'
+    },
+    'torrent-status': {
+      success: 'Trusted',
+      danger: 'Remake'
+    }
+  }
+}
+
+const functions = {}
+
+export default { ...strings, ...functions }
---
+++ locales/ru.ts
@@ -0,0 +1,20 @@
+const strings = {
+  test: 'Русская локаль',
+  'search-torrents': 'Поиск торрентов',
+  nyaa: {
+    'sub-category': {
+      '1_1': 'AMV (Музыкальное видео)',
+      '1_2': 'Английский',
+      '1_3': 'Другой перевод',
+      '1_4': 'Без перевода'
+    },
+    'torrent-status': {
+      success: 'Проверенный',
+      danger: 'Ремейк'
+    }
+  }
+}
+
+const functions = {}
+
+export default { ...strings, ...functions }
