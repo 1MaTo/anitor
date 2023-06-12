42a71d98413bb7f8aca702af4c630f4c6ed0e4e7
MaTo <ily4368@gmail.com>
2023-06-12 15:08:03 +0300

    new api

--- server/api/nyaa/search.ts
+++ server/api/nyaa/search.ts
@@ -1,12 +1,22 @@
 import { si } from 'nyaapi'
+import { DEFAULT_NYAA_SEARCH_TERMS, NyaaQuery } from '../../utils'
 
-export default eventHandler(async () => {
+export default eventHandler(async (event) => {
+  const query = getQuery(event) as NyaaQuery
+
+  const terms = {
+    ...DEFAULT_NYAA_SEARCH_TERMS,
+    n: Number(query.count) || DEFAULT_NYAA_SEARCH_TERMS.n,
+    term: query.query || DEFAULT_NYAA_SEARCH_TERMS.term
+  } as si.SearchOptionsTerm
+
   try {
-    const result = await si.search('HorribleSubs', 20, {
-      filter: 2
+    const result = await si.search(terms)
+    return result
+  } catch (error: any) {
+    throw createError({
+      statusCode: 400,
+      statusMessage: error.message
     })
-    return { hello: 'good', search: result }
-  } catch (error) {
-    return { hello: 'error', search: [] }
   }
 })
