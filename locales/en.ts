f5fa27466cfc9740c0881e8bb49c047c67a57147
MaTo <ily4368@gmail.com>
2023-06-15 18:38:13 +0300

    New locale values

--- locales/en.ts
+++ locales/en.ts
@@ -1,7 +1,15 @@
 const strings = {
   test: 'English locale',
   'search-torrents': 'Search torrents',
+  leecher: 'Leacher | Leachers',
+  seeder: 'Seeder | Seeders',
+  'upload-date': 'Upload date',
+  'torrent-sub-category': 'Torrent subcategory',
+  size: 'Size',
   nyaa: {
+    trusted: 'Torrents uploaded by trusted users',
+    remake:
+      "Reencode of original release; Remux of another uploader's original release for hardsubbing and/or fixing purposes; Reupload of original release using non-original file names; Reupload of original release with missing and/or unrelated additional files",
     'sub-category': {
       '1_1': 'AMV (Music video)',
       '1_2': 'English',
@@ -12,11 +20,7 @@ const strings = {
       success: 'Trusted',
       danger: 'Remake'
     }
-  },
-  leecher: 'Leacher | Leachers',
-  seeder: 'Seeder | Seeders',
-  'upload-date': 'Upload date',
-  'torrent-sub-category': 'Torrent subcategory'
+  }
 }
 
 const functions = {}
