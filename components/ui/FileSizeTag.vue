18bc78f8a80d2d4c6527a6a5b25f4bddaff07beb
MaTo <ily4368@gmail.com>
2023-06-15 18:38:57 +0300

    New components

---
+++ components/ui/FileSizeTag.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+withDefaults(defineProps<{ fileSize?: string }>(), {
+  fileSize: '0'
+})
+</script>
+
+<template>
+  <v-chip size="small" label prepend-icon="fa-solid fa-file-arrow-down" :title="$t('size')">
+    {{ fileSize }}</v-chip
+  >
+</template>
---
+++ components/ui/TorrentStatusTag.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import { TorrentStatus } from '~/types/nyaa'
+import { useI18n } from 'vue-i18n'
+const { t } = useI18n()
+
+const props = withDefaults(defineProps<{ status?: TorrentStatus | string }>(), {
+  status: TorrentStatus.Default
+})
+
+const statusProps = computed(() => {
+  switch (props.status) {
+    case TorrentStatus.Danger:
+      return {
+        color: 'red',
+        icon: 'fa-solid fa-arrow-rotate-left',
+        textColor: 'text-red-darken-4',
+        title: t('nyaa.remake')
+      }
+
+    case TorrentStatus.Success:
+      return {
+        color: 'green',
+        icon: 'fa-solid fa-user-check',
+        textColor: 'text-green',
+        title: t('nyaa.trusted')
+      }
+
+    default:
+      return { color: '', icon: '', textColor: 'text-blue-accent-1', title: '' }
+  }
+})
+</script>
+
+<template>
+  <v-chip
+    v-if="status !== TorrentStatus.Default"
+    size="small"
+    label
+    :title="statusProps.title"
+    :color="statusProps.color"
+    :prepend-icon="statusProps.icon"
+  >
+    {{ $t(`nyaa.torrent-status.${status}`) }}
+  </v-chip>
+</template>
