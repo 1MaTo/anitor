12ed0398d5de9b53381bea2ffae1195043520732
MaTo <ily4368@gmail.com>
2023-06-15 21:52:41 +0300

    New component

---
+++ components/ui/CompleteDownloadsTag.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+withDefaults(defineProps<{ count?: number }>(), { count: 0 })
+</script>
+
+<template>
+  <v-chip :title="$t('nyaa.all-downloads')" size="small" label prepend-icon="fa-solid fa-download">
+    {{ count }}
+  </v-chip>
+</template>
