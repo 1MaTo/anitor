85ce60fe9266ee7e9d441bd872efe09c4ee8fa13
MaTo <ily4368@gmail.com>
2023-06-15 21:52:54 +0300

    Update component

--- components/ui/DateTag.vue
+++ components/ui/DateTag.vue
@@ -3,13 +3,7 @@ withDefaults(defineProps<{ date?: Date }>(), { date: () => new Date() })
 </script>
 
 <template>
-  <v-chip
-    :title="$t('upload-date')"
-    class=""
-    size="small"
-    label
-    prepend-icon="fa-regular fa-calendar"
-  >
+  <v-chip :title="$t('upload-date')" size="small" label prepend-icon="fa-regular fa-calendar">
     {{ date.toLocaleString() }}
   </v-chip>
 </template>
