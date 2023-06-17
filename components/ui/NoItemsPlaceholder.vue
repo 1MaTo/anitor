96a2f8feb77bed8de9cf13f00bd5fa6d0a7ab565
MaTo <ily4368@gmail.com>
2023-06-17 19:29:57 +0300

    No items placeholder component

---
+++ components/ui/NoItemsPlaceholder.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+withDefaults(defineProps<{ label?: string }>(), { label: 'No items found' })
+</script>
+
+<template>
+  <v-sheet border class="pa-10 d-flex justify-center cursor-default">
+    <div class="text-h6 font-weight-regular text-grey">
+      {{ label }}
+    </div>
+  </v-sheet>
+</template>
