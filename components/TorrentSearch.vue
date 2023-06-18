42d0063ba474376c459cd5f6148c0ae12b3459c2
MaTo <ily4368@gmail.com>
2023-06-18 13:28:11 +0300

    Change logic to fetch torrents in store

--- components/TorrentSearch.vue
+++ components/TorrentSearch.vue
@@ -1,26 +1,12 @@
 <script setup lang="ts">
-import { NyaaQuery } from '~/server/utils'
 import { useTorrentStore } from '~/store/useTorrentStore'
 
 const store = useTorrentStore()
 
-const loading = ref(false)
-const errorMessage = ref('')
 const searchText = ref('')
 
-const handleFetchTorrents = async (searchString: string) => {
-  loading.value = true
-  try {
-    const fetchedTorrents = await $fetch('/api/nyaa/search', {
-      query: { query: searchString } as NyaaQuery
-    })
-    store.setTorrents(fetchedTorrents)
-  } catch (error: any) {
-    errorMessage.value = error.message
-    store.setTorrents([])
-  }
-  searchText.value = searchText.value
-  loading.value = false
+const handleSetSearchText = async (searchString: string) => {
+  store.setSearchText(searchString)
 }
 </script>
 
@@ -31,16 +17,16 @@ const handleFetchTorrents = async (searchString: string) => {
         <ui-search-field
           size="big"
           v-model="searchText"
-          :loading="loading"
-          @submit="handleFetchTorrents"
+          :loading="store.loadingTorrents"
+          @submit="handleSetSearchText"
         />
       </v-col>
     </v-row>
     <v-row no-gutters>
       <v-col>
         <v-slide-y-transition leave-absolute>
-          <div v-if="errorMessage" class="text-caption mt-1 text-red-lighten-1">
-            {{ `${$t('nyaa.request-failed')}: ${errorMessage}` }}
+          <div v-if="store.errorMsg" class="text-caption mt-1 text-red-lighten-1">
+            {{ `${$t('nyaa.request-failed')}: ${store.errorMsg}` }}
           </div>
         </v-slide-y-transition>
       </v-col>
--- store/useTorrentStore.ts
+++ store/useTorrentStore.ts
@@ -1,11 +1,47 @@
 import { si } from 'nyaapi'
+import { NyaaQuery } from '~/server/utils'
 
 export const useTorrentStore = defineStore('torrent', () => {
   const torrents = ref<si.Torrent[]>([])
+  const loadingTorrents = ref(false)
+  const errorMsg = ref('')
 
-  const setTorrents = (newTorrents: si.Torrent[]) => {
-    torrents.value = newTorrents
+  const searchText = ref('')
+  const blueRayFilter = ref(true)
+
+  watch([searchText, blueRayFilter], async ([newText, newBlueRayFilter]) => {
+    loadingTorrents.value = true
+    try {
+      const fetchedTorrents = await $fetch('/api/nyaa/search', {
+        query: { query: searchText.value } as NyaaQuery
+      })
+      torrents.value = fetchedTorrents
+    } catch (error: any) {
+      errorMsg.value = error.message
+      torrents.value = []
+    } finally {
+      loadingTorrents.value = false
+    }
+  })
+
+  const setSearchText = (value: string) => {
+    searchText.value = value
   }
 
-  return { torrents, setTorrents }
+  const setTorrentFilter = (value: boolean) => {
+    blueRayFilter.value = value
+  }
+
+  return {
+    torrents,
+
+    blueRayFilter,
+    searchText,
+
+    loadingTorrents,
+    errorMsg,
+
+    setSearchText,
+    setTorrentFilter
+  }
 })
