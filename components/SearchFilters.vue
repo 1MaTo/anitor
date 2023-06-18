87d384bb15823948ed18e20d8bc630653b471006
MaTo <ily4368@gmail.com>
2023-06-18 14:58:22 +0300

    Rework logic with store Move switch to ui component

--- components/SearchFilters.vue
+++ components/SearchFilters.vue
@@ -1,34 +1,29 @@
 <script setup lang="ts">
-const bdFilter = ref(true)
+import { useTorrentStore } from '~/store/useTorrentStore'
+
+const store = useTorrentStore()
+
+const handleUpdateBluRayFilter = (value: boolean): any => {
+  store.setBluRayFilter(value)
+}
+
+const loading = computed(() =>
+  store.loadingTorrents ? (store.bluRayFilter ? 'white' : 'orange-darken-1') : false
+)
 </script>
 
 <template>
   <v-container class="d-flex pa-0 justify-end" fluid>
     <v-row no-gutters justify="end" style="height: 40px">
       <v-col class="flex-grow-0">
-        <v-switch
-          color="blue"
-          class="d-inline-flex flex-grow-0 justify-end switch"
-          hide-details
-          v-model="bdFilter"
-          label="Only Blue-ray"
-        >
-          <template #label="props">
-            <div class="no-wrap">{{ props.label }}</div>
-          </template>
-        </v-switch>
+        <ui-inline-switch
+          :label="$t('only-blue-ray')"
+          :readonly="store.loadingTorrents"
+          :loading="loading"
+          :model-value="store.bluRayFilter"
+          @update:model-value="(event) => handleUpdateBluRayFilter(event as any)"
+        />
       </v-col>
     </v-row>
   </v-container>
 </template>
-
-<style scoped>
-.switch,
-.switch :deep(.v-selection-control) {
-  min-height: 0px;
-}
-
-.no-wrap {
-  white-space: nowrap;
-}
-</style>
---
+++ components/ui/InlineSwitch.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+defineProps<{
+  readonly?: boolean
+  loading?: string | boolean
+  modelValue?: string | boolean
+  label: string
+}>()
+
+defineEmits<{
+  (e: 'update:modelValue', payload: string | boolean): void
+}>()
+</script>
+
+<template>
+  <v-switch
+    :readonly="readonly"
+    :loading="loading"
+    :model-value="modelValue"
+    :label="label"
+    color="primary"
+    class="d-inline-flex flex-grow-0 justify-end switch"
+    hide-details
+    @update:model-value="(event) => $emit('update:modelValue', event)"
+  >
+    <template #label="props">
+      <div class="no-wrap">{{ props.label }}</div>
+    </template>
+  </v-switch>
+</template>
+
+<style scoped>
+.switch,
+.switch :deep(.v-selection-control) {
+  min-height: 0px;
+}
+
+.no-wrap {
+  white-space: nowrap;
+}
+</style>
