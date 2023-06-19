805767c56d7d26bcd952d42567abbfb4c3db5c2b
MaTo <ily4368@gmail.com>
2023-06-19 22:24:00 +0300

    New global classes

--- app.vue
+++ app.vue
@@ -50,4 +50,16 @@ a {
 .cursor-default {
   cursor: default;
 }
+
+.text-clip {
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+}
+
+.no-select {
+  -webkit-user-select: none; /* Safari */
+  -ms-user-select: none; /* IE 10 and IE 11 */
+  user-select: none; /* Standard syntax */
+}
 </style>
