7578f8157eaa0a24d76f13d9d7d1c55f4da47661
MaTo <ily4368@gmail.com>
2023-06-22 19:53:50 +0300

    Move to another file

--- utils/nyaa.ts
+++ utils/nyaa.ts
@@ -63,12 +63,3 @@ export const resolveSortFieldToNyaaSortField = (field?: keyof si.Torrent) => {
       return undefined
   }
 }
-
-export const removeUndefinedFields = (object: any) => {
-  const newObj = {} as any
-  Object.keys(object).forEach((key) => {
-    if (object[key] === Object(object[key])) newObj[key] = removeUndefinedFields(object[key])
-    else if (object[key] !== undefined) newObj[key] = object[key]
-  })
-  return newObj
-}
